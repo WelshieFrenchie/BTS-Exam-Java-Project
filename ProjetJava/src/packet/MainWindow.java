c6d7556143e80e64e9ff65bc23e1a7ea1568dec2
WelshieFrenchie <156076125+WelshieFrenchie@users.noreply.github.com>
2024-04-09 17:31:53 +0200

    v0.0.1
    Ajout du bouton Inscription + ajout de GridBoxLayout pour les boutons Inscription + Connection

--- ProjetJava/src/packet/MainWindow.java
+++ ProjetJava/src/packet/MainWindow.java
@@ -12,7 +12,9 @@ public class MainWindow extends JFrame implements ActionListener{
 	private JPanel boxLeftName = new JPanel();
 	private JScrollPane boxMiddle = new JScrollPane();
 	private JPanel boxRight = new JPanel();
+	private JPanel connectionBox = new JPanel();
 	private JButton loginButton = new JButton("Connexion");
+	private JButton signupButton = new JButton("Inscription");
 	private Container contenu;
 	
 	public MainWindow() {
@@ -29,7 +31,7 @@ public class MainWindow extends JFrame implements ActionListener{
 		this.contenu.add(boxRight);
 		this.contenu.setLayout(null);
 		
-		this.boxLeft.setBounds(20, 50, 300, 525);
+		this.boxLeft.setBounds(20, 50, 300,495);
 		this.boxLeftName.setBounds(20, 20, 300, 30);
 		this.boxLeftName.setBackground(Color.lightGray);
 		this.boxLeftName.add(boxLeftLabel, "align center");
@@ -38,8 +40,26 @@ public class MainWindow extends JFrame implements ActionListener{
 		
 		this.boxRight.setBounds(660, 20, 200, 525);
 		this.boxRight.setBackground(Color.lightGray);
+		this.boxRight.add(connectionBox);
+		this.connectionBox.setBackground(Color.lightGray);
+		this.connectionBox.setBounds(5, 5, 290, 500);
+		
+		GridBagLayout rightBoxGrid = new GridBagLayout();
+		GridBagConstraints rightBoxConst = new GridBagConstraints();
+		this.connectionBox.setLayout(rightBoxGrid);
+		rightBoxConst.gridheight = 1;
+		rightBoxConst.gridwidth = 0;
+		rightBoxConst.gridx = 0;
+		rightBoxConst.gridy = 0;
+		rightBoxConst.insets = new Insets(5,0,5,0);
+		rightBoxGrid.setConstraints(loginButton, rightBoxConst);
+		rightBoxConst.gridy = 1;
+		rightBoxGrid.setConstraints(signupButton, rightBoxConst);
+		
 		this.loginButton.addActionListener(this);
-		this.boxRight.add(loginButton);
+		this.connectionBox.add(loginButton);
+		this.signupButton.addActionListener(this);
+		this.connectionBox.add(signupButton);
 	}
 
 	public void actionPerformed(ActionEvent e) {
